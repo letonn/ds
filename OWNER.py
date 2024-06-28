ebb26772b4a628eea650d60a48e7ac5ac2ada17e
letonn <164564472+letonn@users.noreply.github.com>
2024-06-28 19:59:44 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
-OWNER = ["ELHYBA"]
-OWNER_NAME = "mody"
-BOT_TOKEN = "6768374873:AAGuOBIEQQ2Hw7xyIlUDQVZk6YgKoehJV6w"
-DATABASE = "mongodb+srv://albolbol:albolbolzpy@cluster0.cpd7rm5.mongodb.net/?retryWrites=true&w=majority"
-CHANNEL = "https://t.me/Source_Ze"
-GROUP = "https://t.me/ZeSupport"
-VIDEO = "https://telegra.ph/file/7065a4c7cbedef8befdb9.jpg"
+OWNER = ["S_1_02"]
+OWNER_NAME = "teto"
+BOT_TOKEN = "7093409421:AAE7t0x7MXozli_o2BpMgDDZZm4V8665To4"
+DATABASE = "mongodb+srv://hymbnlnlhyrwkw:Y0AcSx0nVf1jDv4L@cluster0.pjk3hry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+CHANNEL = "https://t.me/A1DIIU"
+GROUP = "https://t.me/ll0llld"
+VIDEO = "https://graph.org/file/061951daec85b0f94b83c.jpg"
 LOGS = "zemaker1"
 
