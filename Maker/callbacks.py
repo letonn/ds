1d1156fcd80bc6fc82442d13c34338d145ad3c87
letonn <164564472+letonn@users.noreply.github.com>
2024-06-28 20:15:24 +0300

    Update callbacks.py

--- Maker/callbacks.py
+++ Maker/callbacks.py
@@ -32,4 +32,4 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             await callback_query.message.reply(ERROR_MESSAGE.format(str(e)))
 
 
-ERROR_MESSAGE = "التواصل : @UP_UO"
+ERROR_MESSAGE = "التواصل : @S_1_02"
