eb2737f1a5bf7e4122898849172d38b310e9c75f
letonn <164564472+letonn@users.noreply.github.com>
2024-06-28 20:21:42 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["S_1_02"]
 OWNER_NAME = "teto"
-BOT_TOKEN = "7093409421:AAE7t0x7MXozli_o2BpMgDDZZm4V8665To4"
+BOT_TOKEN = "7093409421:AAHd8Vdhine-5Z8eHvdF7pivrhJCbSfeAgQ"
 DATABASE = "mongodb+srv://hymbnlnlhyrwkw:Y0AcSx0nVf1jDv4L@cluster0.pjk3hry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 CHANNEL = "https://t.me/A1DIIU"
 GROUP = "https://t.me/ll0llld"
